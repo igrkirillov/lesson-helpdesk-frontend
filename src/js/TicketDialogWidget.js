072e19377b112655fa8a025ccc67d5c2340b180a
Igor.Kirillov <Igor.Kirillov@x5.ru>
2024-05-04 15:23:36 +0300

    frontend

--- src/js/TicketDialogWidget.js
+++ src/js/TicketDialogWidget.js
@@ -5,20 +5,22 @@ export default class TicketDialogWidget {
 
   createElement(ownerElement) {
     const element = document.createElement("div");
-    element.classList.add("ticket-dialog");
+    element.classList.add("ticket-dialog-base");
     element.innerHTML = `
-    <div class="ticket-dialog-title">
-        <span>Добавить тикет</span>
-    </div>
-    <div class="ticket-dialog-container">
-        <label for="name">Краткое описание</label>>
-        <input class="ticket-dialog-name" type="text" name="name">
-        <label for="description">Подробное описание</label>>
-        <textarea class="ticket-dialog-description" type="text" name="description"/>
-        <div class="ticket-dialog-buttons">
-            <input class="ticket-dialog-cancel" type="submit" value="Отмена">
-            <input class="ticket-dialog-ok" type="submit" value="Ок">
-        </div>
+    <div class="ticket-dialog">
+      <div class="ticket-dialog-title">
+          Добавить тикет
+      </div>
+      <div class="ticket-dialog-container">
+          <label for="name">Краткое описание</label>
+          <input class="ticket-dialog-name" type="text" name="name">
+          <label for="description">Подробное описание</label>
+          <textarea class="ticket-dialog-description" name="description"></textarea>
+          <div class="ticket-dialog-buttons">
+              <input class="ticket-dialog-cancel" type="submit" value="Отмена">
+              <input class="ticket-dialog-ok" type="submit" value="Ок">
+          </div>
+      </div>
     </div>`;
     ownerElement.appendChild(element);
     return element;
